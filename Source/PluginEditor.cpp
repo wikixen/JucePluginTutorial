098557286bccc117db92bef4b4c78e879b86e980
Ben <bennylopezhernandez0@gmail.com>
2025-12-09 19:54:45 -0500

    Created a slider

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -13,9 +13,16 @@
 Plugin_tutorialAudioProcessorEditor::Plugin_tutorialAudioProcessorEditor (Plugin_tutorialAudioProcessor& p)
     : AudioProcessorEditor (&p), audioProcessor (p)
 {
-    // Make sure that before the constructor has finished, you've set the
-    // editor's size to whatever you need it to be.
     setSize (400, 300);
+
+    // Slider parameters
+    midiVolume.setSliderStyle(juce::Slider::LinearBarVertical);
+    midiVolume.setRange(0.0, 127.0, 1.0);
+    midiVolume.setTextBoxStyle(juce::Slider::NoTextBox, false, 90, 0);
+    midiVolume.setPopupDisplayEnabled(true, false, this);
+    midiVolume.setTextValueSuffix(" Volume");
+    midiVolume.setValue(1.0);
+    addAndMakeVisible(&midiVolume);
 }
 
 Plugin_tutorialAudioProcessorEditor::~Plugin_tutorialAudioProcessorEditor()
@@ -30,11 +37,10 @@ void Plugin_tutorialAudioProcessorEditor::paint (juce::Graphics& g)
 
     g.setColour (juce::Colours::white);
     g.setFont (juce::FontOptions (15.0f));
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+    g.drawFittedText ("Midi Volume", 0, 0, getWidth(), 30, juce::Justification::centred, 1);
 }
 
 void Plugin_tutorialAudioProcessorEditor::resized()
 {
-    // This is generally where you'll want to lay out the positions of any
-    // subcomponents in your editor..
+    midiVolume.setBounds(40, 30, 20, getHeight() - 60);
 }
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -28,5 +28,6 @@ private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     Plugin_tutorialAudioProcessor& audioProcessor;
+    juce::Slider midiVolume;
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (Plugin_tutorialAudioProcessorEditor)
 };
